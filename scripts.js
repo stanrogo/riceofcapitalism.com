82413f22c7aa9701d76833345fb183573cdfeb07
Stanley Clark <stanley.clark@perplex.nl>
2025-11-08 17:06:13 +0000

    feat: adjust script file to scroll properly

--- scripts.js
+++ scripts.js
@@ -1,65 +1,43 @@
-document.addEventListener('scroll', function(){
+document.addEventListener("scroll", function () {
+  var ricePanels = document.getElementsByClassName("panel-rice");
+  var navs = document.getElementsByClassName("about-link");
+  var activePanel = -1;
 
-	var ricePanels = document.getElementsByClassName('panel-rice');
-	var navs = document.getElementsByClassName('about-link');
-	var activePanel = -1;
+  for (var i = 0; i < ricePanels.length; i++) {
+    var panelOffset = ricePanels[i].getBoundingClientRect().top;
+    var clientWidth = document.body.clientWidth;
+    var targetOffset = clientWidth < 769 ? 145 : 90;
 
-	for(var i = 0; i < ricePanels.length; i++){
+    if (panelOffset < targetOffset) {
+      activePanel = i;
+    }
 
-		var panelOffset = ricePanels[i].getBoundingClientRect().top;
+    navs[i].classList.remove("is-active");
+  }
 
-		if(panelOffset < 85){
-
-			activePanel = i;
-		}
-
-		navs[i].classList.remove('is-active');
-	}
-
-	if(activePanel > -1){
-
-		navs[activePanel].classList.add('is-active');
-	}
+  if (activePanel > -1) {
+    navs[activePanel].classList.add("is-active");
+  }
 });
 
-var links = document.getElementsByClassName('js-link');
+var links = document.getElementsByClassName("js-link");
 
-for(var i = 0; i < links.length; i++){
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function (event) {
+    var text = event.target.textContent;
 
-	links[i].addEventListener('click', function(event){
+    var targetElement = document.getElementsByClassName(
+      "js-" + text.toLowerCase() + "-rice"
+    )[0];
 
-		var text = event.target.textContent;
+    var elemOffset = targetElement.offsetTop - 89;
 
-		var targetElement = document.getElementsByClassName('js-' + text.toLowerCase() + '-rice')[0];
-		var elemOffset = targetElement.offsetTop - 90;
+    var clientWidth = document.body.clientWidth;
 
-		var clientWidth = document.body.clientWidth;
+    if (clientWidth < 769) {
+      elemOffset -= 50;
+    }
 
-		if(clientWidth < 769){
-
-			elemOffset-= 50;
-		}
-
-
-		var scrollTop = document.body.scrollTop;
-
-
-		animate(document.body, "scrollTop", "", scrollTop, elemOffset, 500, true);
-
-	});
-}
-
-function animate(elem,style,unit,from,to,time,prop) {
-	if( !elem) return;
-	var start = new Date().getTime(),
-		timer = setInterval(function() {
-			var step = Math.min(1,(new Date().getTime()-start)/time);
-			if (prop) {
-				elem[style] = (from+step*(to-from))+unit;
-			} else {
-				elem.style[style] = (from+step*(to-from))+unit;
-			}
-			if( step == 1) clearInterval(timer);
-		},25);
-	elem.style[style] = from+unit;
+    window.scrollTo({ top: elemOffset, behavior: "smooth" });
+  });
 }
